afaaf78a89c508d216c2f7f5a7bb6e0076dee13f
Edcko <edckoo1612@gmail.com>
2023-05-19 10:12:15 -0600

    Se cambio el API_URL para usarla el app en desarrollo

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import authHeader from './authHeader';
 
-const API_URL = 'http://ec2-18-119-13-4.us-east-2.compute.amazonaws.com:3000/cherry/';
+const API_URL = 'http://localhost:3000/cherry/';
 
 const getEmpleados = async () => {
   
